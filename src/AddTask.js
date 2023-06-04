d4c4e4d1f4824fd039303e12f9db91ff247b42e7
gogoend <gogoend@qq.com>
2023-06-04 10:13:39 +0000

    chore: 将task更新的reducer及task列表，在上层组件中，通过Context，传入到整个程序

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,18 +1,32 @@
-import { useState } from 'react';
+import { useState, useContext } from "react";
+import { TasksDispatchContext } from "./TasksContext.js";
 
-export default function AddTask({ onAddTask }) {
-  const [text, setText] = useState('');
+export default function AddTask() {
+  const tasksDispatch = useContext(TasksDispatchContext);
+  const [text, setText] = useState("");
+
+  const [nextId, setNextId] = useState(3);
+
   return (
     <>
       <input
         placeholder="Add task"
         value={text}
-        onChange={e => setText(e.target.value)}
+        onChange={(e) => setText(e.target.value)}
       />
-      <button onClick={() => {
-        setText('');
-        onAddTask(text);
-      }}>Add</button>
+      <button
+        onClick={() => {
+          setText("");
+          tasksDispatch({
+            type: "added",
+            text,
+            id: nextId,
+          });
+          setNextId(nextId + 1);
+        }}
+      >
+        Add
+      </button>
     </>
-  )
+  );
 }
